d1d6c95ab327e065622f95e44f0dbf88dd77f7f7
Robert <robertrlm16@gmail.com>
2023-10-03 00:58:49 +0200

    PRIMER COMMIT

---
+++ ComentarioVariasLineas2/src/ComentarioVariasLineas2.java
@@ -0,0 +1,13 @@
+
+public class ComentarioVariasLineas2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<6;i++)
+			System.out.println(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+		}
+
+	}
+
+
